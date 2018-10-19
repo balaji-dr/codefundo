d4fa7619619c78e6c940d7134d0225aad45194ab
Balaji D R <drbalaji97@gmail.com>
2018-10-19 13:14:14 +0530

    structured the project for frontend

---
+++ frontend/android/app/src/main/java/com/codefundo/MainApplication.java
@@ -0,0 +1,51 @@
+package com.codefundo;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import reactnative.hotspot.HotspotPackage;
+import reactnative.hotspot.HotspotPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import io.wifi.p2p.WiFiP2PManagerPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new HotspotModule(),
+          new WiFiP2PManagerPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
